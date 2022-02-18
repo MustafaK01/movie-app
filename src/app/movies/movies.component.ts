4e79bb0931dd3680a175da3c35a1b23f1b597842
Mustafa Köroğlu <62333156+MustafaK01@users.noreply.github.com>
2022-02-18 12:08:14 +0300

    Added movie filter for movie search

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,28 +1,66 @@
 import { Component, OnInit } from '@angular/core';
-import { MovieRepository } from '../models/movie.repository';
+import { ActivatedRoute } from '@angular/router';
 import { Movie } from '../models/movieModel';
+import { AlterifyService } from '../services/aleterify.service';
+import { MovieService } from '../services/movie.service';
+
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.css']
+  styleUrls: ['./movies.component.css'],
+  providers:[MovieService]
 })
 export class MoviesComponent implements OnInit {
 
   title:string="Film Listesi";
   searchedMovie:string = "";
-  movie2:Movie[];
-  movies:Movie[];
-  popularMovies:Movie[];
-  movieRepository:MovieRepository;
+  errorMessage:any;
+  filteredMovies:Movie[]=[];
+  movies:Movie[]=[];
   today=new Date();
-  constructor() { 
-    this.movieRepository = new MovieRepository();
-    this.movies = this.movieRepository.getMovies();
-    this.popularMovies = this.movieRepository.getPopularMovies();
-
+  private alertify : AlterifyService;
+  private movieService:MovieService;
+  private activatedRoute:ActivatedRoute;
+  constructor(alertify : AlterifyService,movieService:MovieService,
+    activatedRoute:ActivatedRoute) {
+    this.alertify = alertify;
+    this.movieService = movieService;
+    this.activatedRoute = activatedRoute;
+    this.filteredMovies=this.movies;
   }
+  
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params=>{
+      this.movieService.getMovies(params["categoryId"]).subscribe(data =>{
+        this.movies=data
+        this.filteredMovies=this.movies
+        },(error=>{
+          this.errorMessage=error;
+          this.alertify.error("Hata!");
+        }));
+    });
   }  
 
+  searchMovie(){
+      this.filteredMovies = this.searchedMovie? 
+      this.movies.filter((i:Movie)=>i.title.toLowerCase().includes(this.searchedMovie.toLowerCase())||
+        i.screenWriters.toLowerCase().includes(this.searchedMovie.toLowerCase())||
+        i.filmDirectors.toLowerCase().includes(this.searchedMovie.toLowerCase())):this.movies
+  }
+  addToList($event:any,movie:Movie){
+    if($event.target.classList.contains('btn-primary')){
+      $event.target.innerText = "Listeden Çıkar";
+      $event.target.classList.remove('btn-primary');
+      $event.target.classList.add('btn-danger');
+      this.alertify.success(movie.title+" listeye eklendi");
+    }else{
+      $event.target.innerText = "Listeye Ekle";
+      $event.target.classList.remove('btn-danger');
+      $event.target.classList.add('btn-primary');
+      this.alertify.warning(movie.title+" listeden çıkarıldı");
+    }
+  }
+
 }
+
