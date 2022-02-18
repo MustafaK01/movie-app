9a81dd27840fc9b2015c28d5f758815887a1746a
Mustafa Köroğlu <62333156+MustafaK01@users.noreply.github.com>
2022-02-18 12:11:01 +0300

    category filter added

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,22 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { CategoryRepository } from '../models/category.repository';
 import { Category } from '../models/categoryModel';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
+  providers:[CategoryService]
 })
 export class CategoryComponent implements OnInit {
 
   categories:Category[];
-  categoryRepository:CategoryRepository;  
-  constructor() { 
-    this.categoryRepository = new CategoryRepository();
-    this.categories=this.categoryRepository.getCategory();
+  categoryService:CategoryService;
+  constructor(categoryService:CategoryService) { 
+    this.categoryService=categoryService;
   }
 
   ngOnInit(): void {
+    this.categoryService.getCategories().subscribe(data=>{
+      this.categories=data;
+    })
   }
 
 }
