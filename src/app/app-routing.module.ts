2e6214f7ea3393c3881db18057c844e0f5a76f4d
Mustafa Köroğlu <62333156+MustafaK01@users.noreply.github.com>
2022-02-19 10:24:16 +0300

    Added new route for movie-add component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { MoviesComponent } from './movies/movies.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { MovieAddComponent } from './movie-add/movie-add.component';
 
 
 const routes:Routes = [
   {path:'movies',component:MoviesComponent},
   {path:'',redirectTo:'movies',pathMatch:'full'},
-  
   {path:'movies/category/:categoryId',component:MoviesComponent},
-  {path:'movies/category',redirectTo:'movies',pathMatch:'full'},
 
+  {path:'movies/category',redirectTo:'movies',pathMatch:'full'},
+  
+  {path:'movies/add',component:MovieAddComponent},
   {path:'movies/:movieId',component:MovieDetailsComponent}
 
 ];
